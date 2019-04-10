4bae6e33e254e0c674534b8753ce25eb801610f4
sacosta8 <sacosta8@binghamton.edu>
2019-04-10 18:45:01 -0400

    Progress? Joining the threads might be the issue

--- readwrite.cpp
+++ readwrite.cpp
@@ -41,6 +41,7 @@ public:
 
 	void insert(int data){
 		Node * node = new Node(data);
+		std::cout << data << std::endl;
 		if(head == NULL){
 			head = node;
 			return;
@@ -61,6 +62,14 @@ struct locksAndCounts {
 	sem_t wmutex;
 	sem_t readTry;
 	sem_t resource;
+	locksAndCounts(){
+		readcount = 0;
+		writecount = 0;
+		sem_init(&rmutex,0,1);
+		sem_init(&wmutex,0,1);
+		sem_init(&readTry,0,1);
+		sem_init(&resource,0,1);
+	}
 };
 
 struct readerArgs {
@@ -73,7 +82,6 @@ struct readerArgs {
 struct writerArgs {
 	int writerNumber;
 	int n;
-	int data;
 	LinkedList * list;
 	locksAndCounts * lac;
 };
@@ -88,50 +96,62 @@ void * reader( void * arguments){
 	file.open(filename.c_str(),std::fstream::out);
 	for(int i = 0; i < args->n; i++){		
 		sem_wait(&lac->readTry);	
-		sem_wait(&lac->rmutex);
-		lac->readcount++;
-		if(lac->readcount == 1)
-			sem_wait(&lac->resource);
-		sem_post(&lac->rmutex);
+			sem_wait(&lac->rmutex);
+			lac->readcount++;
+			int value;
+			sem_getvalue(&lac->rmutex,&value);
+			std::cout << "rmutex: " << value;
+			if(lac->readcount == 1)
+				sem_wait(&lac->resource);
+			sem_post(&lac->rmutex);
 		sem_post(&lac->readTry);
 		int count = args->list->traverse(args->readerNumber);
-		file << "Reader " << args->readerNumber << ": " << "Read " << i << ": " << count << "values ending in " << args->readerNumber;
+		std::string message = "Reader " + std::to_string(args->readerNumber) + ": Read " + std::to_string(i) + ": " + std::to_string(count) + " values ending in " + std::to_string(args->readerNumber);
+		file << message;
 		sem_wait(&lac->rmutex);
 		lac->readcount--;
 		if (lac->readcount == 0)
 			sem_post(&lac->resource);
 		sem_post(&lac->rmutex);
-		struct timespec req = {0};
-		req.tv_sec = 0;
-		req.tv_nsec = 1000000L; //Will go to sleep for 1 millisecond 
-		nanosleep(&req,(struct timespec *)NULL);
 	}
 
 	file.close();
+
 }
 
 void * writer(void * arguments){
 	writerArgs * args = (writerArgs *) arguments;
 	locksAndCounts * lac = args->lac;
 	for(int i = 0; i < args->n; i++){
+		//To make writer random number
+		int randNum = rand() % 1000 + 1;
+		int difference = (randNum % 10) - (i+1);
+		if(difference > 0)
+			randNum -= difference;
+		if(difference < 0)
+			randNum += difference * -1;
 		sem_wait(&lac->wmutex);
 		lac->writecount++;
 		if (lac->writecount == 1)
 			sem_wait(&lac->readTry);
 		sem_post(&lac->wmutex);
 		sem_wait(&lac->resource);
-		args->list->insert(args->data);
+		args->list->insert(randNum);
 		sem_post(&lac->resource);
 		sem_wait(&lac->wmutex);
 		lac->writecount++;
 		if (lac->writecount == 0)
 			sem_wait(&lac->readTry);
 		sem_post(&lac->wmutex);
-		struct timespec req = {0};
-		req.tv_sec = 0;
-		req.tv_nsec = 1000000L; //Will go to sleep for 1 millisecond 
-		nanosleep(&req,(struct timespec *)NULL);
 	}
+	int value;
+	sem_getvalue(&lac->wmutex,&value);
+	std::cout << "wmutex: " << value;
+	sem_getvalue(&lac->resource,&value);
+	std::cout << "resource: " << value;
+	sem_getvalue(&lac->readTry,&value);
+	std::cout << "readTry: " << value;
+	
 
 }
 
@@ -147,13 +167,7 @@ int main(int argc, char * argv[]){
 		w = atoi(argv[3]);
 		if ( ( (n >= 1) && (n <= 100) ) && ( (r >= 1) && (r <= 9) ) && ( (w >= 1) && (w <= 9) ) ){
 			LinkedList * list = (LinkedList *) malloc(sizeof(LinkedList));
-			locksAndCounts * lac = (locksAndCounts *) malloc(sizeof(LinkedList));
-			lac->readcount = 0;
-			lac->writecount = 0;
-			sem_init(&lac->rmutex,0,1);
-			sem_init(&lac->wmutex,0,1);
-			sem_init(&lac->readTry,0,1);
-			sem_init(&lac->resource,0,1);
+			locksAndCounts * lac = new locksAndCounts();
 			pthread_t readerThreads[r];
 			pthread_t writerThreads[w];
 			for(int i = 0; i < w; i++){
@@ -162,14 +176,6 @@ int main(int argc, char * argv[]){
 				args->n = n;
 				args->list = list;
 				args->lac = lac;
-				//To make writer random number
-				int randNum = rand() % 1000 + 1;
-				int difference = (randNum % 10) - (i+1);
-				if(difference > 0)
-					randNum -= difference;
-				if(difference < 0)
-					randNum += difference * -1;
-				args->data = randNum;
 				pthread_create(&writerThreads[i],NULL,writer, (void *) args);
 			}
 			for(int i = 0; i < r; i++){
@@ -180,6 +186,10 @@ int main(int argc, char * argv[]){
 				args->lac = lac;
 				pthread_create(&readerThreads[i],NULL,reader, (void *) args);
 			}
+			for(int i = 0; i < w;i++)
+				pthread_join(writerThreads[i],NULL);
+			for(int i = 0; i < r;i++)
+				pthread_join(readerThreads[i],NULL);
 
 		}
 		else {
